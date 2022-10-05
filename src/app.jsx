87fa8948ad31a1238fe521112cafbcb3c2d4dc20
Luis Torres <luiscar241@gmail.com>
2022-10-05 12:43:47 -0300

    Se agrega el proyecto

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,27 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import './app.css'
-
-export function App() {
-  const [count, setCount] = useState(0)
-
+import { useState, useEffect } from 'react'
+import './App.css'
+function App() {
+  const [paises, setPaises] = useState([]);
+  const fetchapi = async () => {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    setPaises(await response.json());
+  }
+  useEffect(() => {
+    fetchapi()
+  })
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <ul>
+      {paises.map((pais) => (
+          <li key={pais.name.common}>
+            <p>Pais: {pais.name.official + " "}</p>
+            <p>Capital: {pais.capital + " "}</p>
+            <img src = {pais.flags.png } alt = "bandera"/>
+
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
+export default App
